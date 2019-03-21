0990865a4b3f81fcc9686e97e1e5d9a334297beb
sarkis74 <sarkis_aghazarian@yahoo.com>
2019-03-20 19:48:19 -0700

    latest

--- auth-server/src/auth/oauth/twitch.js
+++ auth-server/src/auth/oauth/twitch.js
@@ -1,37 +1,36 @@
 'use strict';
-
+// const express = require('express');
+// const app = express();
 const superagent = require('superagent');
 const Users = require('../users-model.js');
+// require('dotenv').config();
 
-const API = 'http://localhost:3000';
-const TWITCH = 'https://id.twitch.tv/oauth2/authorize';
 const SERVICE = 'https://id.twitch.tv/oauth2/token';
-const REDIRECT = 'http://localhost:3000/oauth';
+const PROFILE = 'https://api.twitch.tv/helix/users';
+// TODO: Change this REDIRECT when we deploy on Heroku
 
 let authorize = (request) => {
-
+    // the authorization code is in query.code because of the ?code= in the URL
     console.log('(1)', request.query.code);
 
-    return superagent.get(`${TWCH}?client_id=${process.env}`)
-        .type('form')
-        // .send({
-        //     code: request.query.code,
-        //     client_id: process.env.GOOGLE_CLIENT_ID,
-        //     client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        //     redirect_uri: `${API}/oauth`,
-        //     grant_type: 'authorization_code',
-        // })
+    // Using the Auth code to perform a 'handshake'
+    return superagent.post(`${SERVICE}?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&code=${request.query.code}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT_URI}`)
+    // We should now receive back a JSON object
+    // access_token, refresh_token, expires_in, scope:[], token_type
         .then( response => {
             let access_token = response.body.access_token;
-            console.log('(2)', access_token);
+            console.log('(2: access token)', access_token);
+            console.log('(2.5: scope) ', response.body.scope);
             return access_token;
         })
+
+        // We're here now
         .then(token => {
-            console.log(SERVICE, token);
-            return superagent.get(SERVICE)
+            console.log(PROFILE, token);
+            return superagent.get(PROFILE)
                 .set('Authorization', `Bearer ${token}`)
                 .then( response => {
-                    let user = response.body;
+                    let user = response.body.data[0];
                     console.log('(3)', user);
                     return user;
                 });
--- auth-server/src/auth/router.js
+++ auth-server/src/auth/router.js
@@ -8,28 +8,29 @@ const auth = require('./middleware.js');
 const oauth = require('./oauth/twitch.js');
 
 authRouter.post('/signup', (req, res, next) => {
-  let user = new User(req.body);
-  user.save()
-    .then( (user) => {
-      req.token = user.generateToken();
-      req.user = user;
-      res.set('token', req.token);
-      res.cookie('auth', req.token);
-      res.send(req.token);
-    }).catch(next);
+    let user = new User(req.body);
+    user.save()
+        .then( (user) => {
+            req.token = user.generateToken();
+            req.user = user;
+            res.set('token', req.token);
+            res.cookie('auth', req.token);
+            res.send(req.token);
+        }).catch(next);
 });
 
 authRouter.post('/signin', auth, (req, res, next) => {
-  res.cookie('auth', req.token);
-  res.send(req.token);
+    res.cookie('auth', req.token);
+    res.send(req.token);
 });
 
 authRouter.get('/oauth', (req,res,next) => {
-  oauth(req)
-    .then( token => {
-      res.status(200).send(token);
-    })
-    .catch(next);
+    // :code is the authorization code returned from Twitch
+    oauth(req)
+        .then( token => {
+            res.status(200).send(token);
+        })
+        .catch(next);
 });
 
 module.exports = authRouter;
